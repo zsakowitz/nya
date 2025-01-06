a5694efcab737e694ee5a708effa66120bb65506
sakawi <65452710+zsakowitz@users.noreply.github.com>
2025-01-05 22:26:11 -0800

    Stop doing weird stuff with CmdMap

--- src/editor/cmd/util/map.ts
+++ src/editor/cmd/util/map.ts
@@ -1,26 +1,28 @@
-import type { Cursor, Init, InitRet, Selection } from "../../model"
+import {
+  performInit,
+  type Cursor,
+  type Init,
+  type InitRet,
+  type Selection,
+} from "../../model"
 import type { Options } from "../../options"
 
 export class CmdMap<E> implements Init<E> {
   constructor(
     readonly fn: Init<E>,
     readonly tx: (input: string) => string,
-  ) {
-    if (!fn.initOn) {
-      this.initOn = undefined
-    }
-  }
+  ) {}
 
   init(cursor: Cursor, input: string, options: Options, event: E): InitRet {
     return this.fn.init(cursor, this.tx(input), options, event)
   }
 
-  initOn?(
+  initOn(
     selection: Selection,
     input: string,
     options: Options,
     event: E,
   ): InitRet {
-    return this.fn.initOn!(selection, this.tx(input), options, event)
+    return performInit(this.fn, selection, this.tx(input), options, event)
   }
 }
