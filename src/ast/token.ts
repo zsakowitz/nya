f8f93dd7ae5da066f4b626873402cec8ffdb8849
sakawi <65452710+zsakowitz@users.noreply.github.com>
2025-01-07 00:58:42 -0800

    things

--- src/ast/token.ts
+++ src/ast/token.ts
@@ -26,8 +26,39 @@ export type PuncBinary =
 /** A punctuation token which represents a unary operator. */
 export type PuncUnary = "\\neg " | PuncPm | "!"
 
-/** Negative precedence */
-export type PuncPrecedence = 2
+/** Higher is tighter. See the source for reserved precedence levels. */
+export type PuncPrecedence = Exclude<
+  | 15 // factorials                       23!
+  | 14 // member access                    [1, 2, 3].min
+  | 13 // explicit function calls          sin(23)
+  | 12 // exponentials [future expansion]  x ↑ 3
+  | 11 // implicit multiplication          xy
+  | 10 // products                         x ÷ y
+  | 9 // implicit function calls           sin 2x³
+  | 8 // summation notation                ∑n²
+  | 7 // implicit multiplication again     2∑n²
+  | 6 // implicit fn calls again           sin 2∑n²
+  | 5 // sums                              2 + 3
+  | 4 // ranges                            1...100
+  | 3 // comparators                       x² < 3
+  | 2 // equality operators                x = 3
+  | 1 // negation                          ¬x
+  | 0, // boolean combinators              x ∧ y
+  13 | 12 | 11 | 9 | 8 | 7 | 6
+>
+
+// rules for implicits to Just Work™:
+// exp = anything held together by operators of precedence "exponential" or higher
+// term = exp+ (fn+ | sum)?
+// prod = term ("*" term)*
+// fn = "sin" (fn | (prod | sum)+)
+// sum = "∑" (prod | fn | sum)+
+
+// Would be awesome if these things Just Worked™:
+// 16!.lcm(5)        (16!).lcm(5)
+// sin a cos b       (sin a)(cos b)
+// sin 2a            sin(2a)
+// 2 ∑n²             (2)(∑n²)
 
 export type Punc =
   | { type: "prefix"; kind: PuncUnary; precedence: PuncPrecedence }
