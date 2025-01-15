7ab62724623edb2ff76d75aa336cd65d5f222828
sakawi <65452710+zsakowitz@users.noreply.github.com>
2025-01-15 15:55:12 -0800

    circle icon

--- src/sheet/index.ts
+++ src/sheet/index.ts
@@ -102,6 +102,7 @@ export class Expr {
 
   readonly el
   readonly elIndex
+  readonly elCircle
   readonly elValue
   readonly elValueError
   readonly elScroller
@@ -119,6 +120,19 @@ export class Expr {
         (this.elIndex = h(
           "text-[65%] [line-height:1] text-slate-500 group-focus-within:text-white",
           "" + this.sheet.exprs.length,
+        )),
+        // prettier-ignore
+        (this.elCircle = h(
+          "relative block bg-white size-8 rounded-full mx-0.5 overflow-clip",
+          h("size-[27.27%] top-[00.00%] left-[00.00%] absolute bg-red-300 rounded-br-[25%]"),
+          h("size-[27.27%] top-[00.00%] left-[36.36%] absolute bg-yellow-300 rounded-b-[25%]"),
+          h("size-[27.27%] top-[00.00%] left-[72.72%] absolute bg-fuchsia-300 rounded-bl-[25%]"),
+          h("size-[27.27%] top-[36.36%] left-[00.00%] absolute bg-blue-300 rounded-r-[25%]"),
+          h("size-[27.27%] top-[36.36%] left-[36.36%] absolute bg-slate-400 rounded-[25%]"),
+          h("size-[27.27%] top-[36.36%] left-[72.72%] absolute bg-green-300 rounded-l-[25%]"),
+          h("size-[27.27%] top-[72.72%] left-[00.00%] absolute bg-slate-300 rounded-tr-[25%]"),
+          h("size-[27.27%] top-[72.72%] left-[36.36%] absolute bg-purple-300 rounded-t-[25%]"),
+          h("size-[27.27%] top-[72.72%] left-[72.72%] absolute bg-orange-300 rounded-tl-[25%]"),
         )),
       ),
       h(
@@ -201,6 +215,8 @@ export class Expr {
       console.error(e)
       this.displayError(e instanceof Error ? e : new Error(String(e)))
     }
+
+    return
 
     try {
       const props = defaultPropsGlsl()
