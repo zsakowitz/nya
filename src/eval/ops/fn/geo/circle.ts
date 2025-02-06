8139885a2de2714e4401377e44538e95b8e21cd6
sakawi <65452710+zsakowitz@users.noreply.github.com>
2025-02-06 07:43:26 -0800

    add glider example

--- src/eval/ops/fn/geo/circle.ts
+++ src/eval/ops/fn/geo/circle.ts
@@ -1,7 +1,31 @@
+import type { GlslContext } from "../../../lib/fn"
+import type { GlslVal, JsVal } from "../../../ty"
 import { num, real } from "../../../ty/create"
 import { FnDist } from "../../dist"
 import { abs } from "../../op/abs"
 import { sub } from "../../op/sub"
+
+function js(a: JsVal<"point32" | "c32">, b: JsVal<"point32" | "c32">) {
+  return {
+    center: a.value,
+    // TODO: use approx and exact better
+    radius: real(
+      Math.hypot(
+        num(sub(a.value.x, b.value.x)),
+        num(sub(a.value.y, b.value.y)),
+      ),
+    ),
+  }
+}
+
+function glsl(
+  ctx: GlslContext,
+  ar: GlslVal<"point32" | "c32">,
+  b: GlslVal<"point32" | "c32">,
+) {
+  const a = ctx.cache(ar)
+  return `vec3(${a}, length(${a} - ${b.expr}))`
+}
 
 export const FN_CIRCLE = new FnDist(
   "circle",
@@ -14,20 +38,12 @@ export const FN_CIRCLE = new FnDist(
     (_, a, b) => `vec3(${a.expr}, abs(${b.expr}))`,
   )
   .add(
-    ["point32", "point32"],
+    ["c32", "r32"],
     "circle32",
-    (a, b) => ({
-      center: a.value,
-      // TODO: use approx and exact better
-      radius: real(
-        Math.hypot(
-          num(sub(a.value.x, b.value.x)),
-          num(sub(a.value.y, b.value.y)),
-        ),
-      ),
-    }),
-    (ctx, ar, b) => {
-      const a = ctx.cache(ar)
-      return `vec3(${a}, length(${a} - ${b.expr}))`
-    },
+    (a, b) => ({ center: a.value, radius: abs(b.value) }),
+    (_, a, b) => `vec3(${a.expr}, abs(${b.expr}))`,
   )
+  .add(["point32", "point32"], "circle32", js, glsl)
+  .add(["point32", "c32"], "circle32", js, glsl)
+  .add(["c32", "point32"], "circle32", js, glsl)
+  .add(["c32", "c32"], "circle32", js, glsl)
