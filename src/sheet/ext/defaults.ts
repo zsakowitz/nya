a87625d8d5240db4f14694690401aba827b34920
sakawi <65452710+zsakowitz@users.noreply.github.com>
2025-02-06 13:06:48 -0800

    lines and rays render properly

--- src/sheet/ext/defaults.ts
+++ src/sheet/ext/defaults.ts
@@ -3,6 +3,7 @@ import { EXT_SLIDER } from "./exts/00-slider"
 import { EXT_CIRCLE } from "./exts/01-circle"
 import { EXT_LINE } from "./exts/01-line"
 import { EXT_POINT } from "./exts/01-point"
+import { EXT_RAY } from "./exts/01-ray"
 import { EXT_SEGMENT } from "./exts/01-segment"
 import { EXT_EVAL } from "./exts/02-eval"
 import { EXT_GLSL } from "./exts/03-shader"
@@ -12,6 +13,7 @@ export const exts = new Exts([
   EXT_POINT,
   EXT_SEGMENT,
   EXT_LINE,
+  EXT_RAY,
   EXT_CIRCLE,
   EXT_EVAL,
   EXT_GLSL,
--- src/sheet/ext/exts/01-ray.ts
+++ src/sheet/ext/exts/01-ray.ts
@@ -3,7 +3,7 @@ import { each, type JsValue, type Tys } from "../../../eval/ty"
 import { num } from "../../../eval/ty/create"
 import type { Paper, Point } from "../../ui/paper"
 
-function getLineBounds(line: Tys["line32"], paper: Paper): [Point, Point] {
+function getRayBounds(line: Tys["ray32"], paper: Paper): [Point, Point] | null {
   const x1 = num(line[0].x)
   const y1 = num(line[0].y)
   const x2 = num(line[1].x)
@@ -11,26 +11,60 @@ function getLineBounds(line: Tys["line32"], paper: Paper): [Point, Point] {
   const { xmin, w, ymin, h } = paper.bounds()
 
   if (x1 == x2) {
+    if (y1 < y2) {
+      if (y1 > ymin + h) {
+        return null
+      }
+      return [
+        paper.paperToCanvas({ x: x1, y: y1 }),
+        paper.paperToCanvas({ x: x1, y: ymin + h }),
+      ]
+    }
+
+    if (y1 < ymin) {
+      return null
+    }
     return [
+      paper.paperToCanvas({ x: x1, y: y1 }),
       paper.paperToCanvas({ x: x1, y: ymin }),
-      paper.paperToCanvas({ x: x1, y: ymin + h }),
     ]
   }
 
   const m = (y2 - y1) / (x2 - x1)
 
+  if (x1 < x2) {
+    if (x1 > xmin + w) {
+      return null
+    }
+
+    return [
+      paper.paperToCanvas({ x: x1, y: y1 }),
+      paper.paperToCanvas({ x: xmin + w, y: m * (xmin + w - x1) + y1 }),
+    ]
+  }
+
+  if (x1 < xmin) {
+    return null
+  }
+
   return [
+    paper.paperToCanvas({ x: x1, y: y1 }),
     paper.paperToCanvas({ x: xmin, y: m * (xmin - x1) + y1 }),
-    paper.paperToCanvas({ x: xmin + w, y: m * (xmin + w - x1) + y1 }),
   ]
+
+  //
+  //   return [
+  //     paper.paperToCanvas({ x: xmin, y: m * (xmin - x1) + y1 }),
+  //     paper.paperToCanvas({ x: xmin + w, y: m * (xmin + w - x1) + y1 }),
+  //   ]
 }
 
-export const EXT_LINE = defineExt({
+export const EXT_RAY = defineExt({
   data(expr) {
     const value = expr.js?.value
 
-    if (value && value.type == "line32") {
-      return { value: value as JsValue<"line32"> }
+    if (value && value.type == "ray32") {
+      return { value: value as JsValue<"ray32"> }
     }
   },
   plot2d(data, paper) {
@@ -43,7 +77,10 @@ export const EXT_LINE = defineExt({
       if (!(isFinite(x1) && isFinite(y1) && isFinite(x2) && isFinite(y2)))
         continue
 
-      const [o1, o2] = getLineBounds(segment, paper)
+      const bounds = getRayBounds(segment, paper)
+      if (!bounds) continue
+      const [o1, o2] = bounds
+
       const { ctx, scale } = paper
 
       ctx.beginPath()
