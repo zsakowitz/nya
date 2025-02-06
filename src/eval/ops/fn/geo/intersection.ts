e3169670727c40096030a953d156c6d0c484d863
sakawi <65452710+zsakowitz@users.noreply.github.com>
2025-02-06 10:24:46 -0800

    tidy up intersections

--- src/eval/ops/fn/geo/intersection.ts
+++ src/eval/ops/fn/geo/intersection.ts
@@ -67,7 +67,7 @@ for (const a of ["line32", "ray32", "segment32", "vector32"] as const) {
   }
 }
 
-function intersect(circ: Tys["circle32"], lin: Tys["line32"], index: 0 | 1) {
+function intersect(circ: Tys["circle32"], lin: Tys["line32"], index: -1 | 1) {
   // https://stackoverflow.com/a/37225895
   const circle = {
     center: { x: num(circ.center.x), y: num(circ.center.y) },
@@ -77,24 +77,21 @@ function intersect(circ: Tys["circle32"], lin: Tys["line32"], index: 0 | 1) {
     p1: { x: num(lin[0].x), y: num(lin[0].y) },
     p2: { x: num(lin[1].x), y: num(lin[1].y) },
   }
-  var b, c, d, u1, u2, v1, v2
-  v1 = { x: line.p2.x - line.p1.x, y: line.p2.y - line.p1.y }
-  v2 = { x: line.p1.x - circle.center.x, y: line.p1.y - circle.center.y }
-  b = v1.x * v2.x + v1.y * v2.y
-  c = 2 * (v1.x * v1.x + v1.y * v1.y)
-  b *= -2
-  d = Math.sqrt(
+  const v1 = { x: line.p2.x - line.p1.x, y: line.p2.y - line.p1.y }
+  const v2 = { x: line.p1.x - circle.center.x, y: line.p1.y - circle.center.y }
+  const b = -2 * (v1.x * v2.x + v1.y * v2.y)
+  const c = 2 * (v1.x * v1.x + v1.y * v1.y)
+  const d = Math.sqrt(
     b * b - 2 * c * (v2.x * v2.x + v2.y * v2.y - circle.radius * circle.radius),
   )
   if (isNaN(d)) {
     // no intercept
     return pt(real(NaN), real(NaN))
   }
-  u1 = (b - d) / c // these represent the unit distance of point one and two on the line
-  u2 = (b + d) / c
+  const u1 = (b + index * d) / c // these represent the unit distance of point one and two on the line
   const returned = {
-    x: line.p1.x + v1.x * (index == 0 ? u1 : u2),
-    y: line.p1.y + v1.y * (index == 0 ? u1 : u2),
+    x: line.p1.x + v1.x * u1,
+    y: line.p1.y + v1.y * u1,
   }
   return pt(real(returned.x ?? NaN), real(returned.y ?? NaN))
 }
@@ -103,7 +100,7 @@ for (const b of ["line32", "ray32", "segment32", "vector32"] as const) {
   FN_INTERSECTION.add(
     ["circle32", b],
     "point32",
-    (circ, lin) => intersect(circ.value, lin.value, 0),
+    (circ, lin) => intersect(circ.value, lin.value, -1),
     () => {
       throw new Error("no line-circle intersections on the GPU yet")
     },
