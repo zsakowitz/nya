610cbba752e16af87cde872c2502a7413c0009a3
sakawi <65452710+zsakowitz@users.noreply.github.com>
2025-02-06 13:48:58 -0800

    vectors

--- src/sheet/ext/defaults.ts
+++ src/sheet/ext/defaults.ts
@@ -5,6 +5,7 @@ import { EXT_LINE } from "./exts/01-line"
 import { EXT_POINT } from "./exts/01-point"
 import { EXT_RAY } from "./exts/01-ray"
 import { EXT_SEGMENT } from "./exts/01-segment"
+import { EXT_VECTOR } from "./exts/01-vector"
 import { EXT_EVAL } from "./exts/02-eval"
 import { EXT_GLSL } from "./exts/03-shader"
 
@@ -12,8 +13,9 @@ export const exts = new Exts([
   EXT_SLIDER,
   EXT_POINT,
   EXT_SEGMENT,
-  EXT_LINE,
   EXT_RAY,
+  EXT_LINE,
+  EXT_VECTOR,
   EXT_CIRCLE,
   EXT_EVAL,
   EXT_GLSL,
---
+++ src/sheet/ext/exts/01-vector.ts
@@ -0,0 +1,54 @@
+import { defineExt } from ".."
+import { each, type JsValue } from "../../../eval/ty"
+import { num } from "../../../eval/ty/create"
+
+export const EXT_VECTOR = defineExt({
+  data(expr) {
+    const value = expr.js?.value
+
+    if (value && value.type == "vector32") {
+      return { value: value as JsValue<"vector32"> }
+    }
+  },
+  plot2d(data, paper) {
+    for (const segment of each(data.value)) {
+      const x1 = num(segment[0].x)
+      const y1 = num(segment[0].y)
+      const x2 = num(segment[1].x)
+      const y2 = num(segment[1].y)
+
+      if (!(isFinite(x1) && isFinite(y1) && isFinite(x2) && isFinite(y2)))
+        continue
+
+      const o1 = paper.paperToCanvas({ x: x1, y: y1 })
+      const o2 = paper.paperToCanvas({ x: x2, y: y2 })
+      const { ctx, scale } = paper
+
+      const dx = o2.x - o1.x
+      const dy = o2.y - o1.y
+      const nx = (12 * scale * dx) / Math.hypot(dx, dy)
+      const ny = (12 * scale * dy) / Math.hypot(dx, dy)
+      const ox = o2.x - nx
+      const oy = o2.y - ny
+      const w = 0.5
+
+      ctx.beginPath()
+      ctx.lineWidth = 3 * scale
+      ctx.strokeStyle = "#2d70b3cc"
+      ctx.moveTo(o1.x, o1.y)
+      ctx.lineTo(ox, oy)
+      ctx.stroke()
+
+      ctx.beginPath()
+      ctx.fillStyle = "#2d70b3cc"
+      ctx.moveTo(o2.x, o2.y)
+      ctx.lineTo(ox + w * ny, oy - w * nx)
+      ctx.lineTo(ox - w * ny, oy + w * nx)
+      ctx.lineTo(o2.x, o2.y)
+      ctx.fill()
+    }
+  },
+  layer() {
+    return 1
+  },
+})
