0926718432ab4fc09506150001309210af9a2010
sakawi <65452710+zsakowitz@users.noreply.github.com>
2025-02-11 11:51:34 -0800

    start on package format

---
+++ src/pkg.ts
@@ -0,0 +1,50 @@
+import type { NodeName, Nodes } from "./eval/ast/token"
+import type { AstTxr } from "./eval/ast/tx"
+import type { Fn } from "./eval/ops"
+import type { Builtin } from "./eval/ops/vars"
+import type { TyComponents, TyName } from "./eval/ty"
+import type { TyInfo } from "./eval/ty/info"
+import type { WordKind } from "./field/cmd/leaf/var"
+import type { LatexInit } from "./field/latex"
+import type { Init } from "./field/model"
+import type { Options } from "./field/options"
+import type { AnyExt } from "./sheet/ext"
+import type { Sheet } from "./sheet/ui/sheet"
+
+export type List<T> = { readonly [x: string]: T }
+
+export interface Package {
+  id: string
+  name: string
+  label: string
+
+  field?: {
+    inits?: List<Init>
+    shortcuts?: List<Init>
+    autos?: List<Init>
+    words?: List<WordKind>
+    latex?: List<LatexInit>
+    options?: Partial<Options>
+  }
+
+  ast?: {
+    ty?: Partial<{ [K in TyName]: TyInfo<K, TyComponents[K]> }>
+    txrs?: Partial<{ [K in NodeName]: AstTxr<Nodes[K]> }>
+    fns?: List<Fn>
+    vars?: List<Builtin>
+    opUnary?: List<Fn>
+    opBinary?: List<Fn>
+  }
+
+  sheet?: {
+    exts?: List<AnyExt>
+    toolbar?: List<ToolbarItem>
+  }
+
+  remarks?: readonly string[]
+}
+
+export interface ToolbarItem {
+  icon(): HTMLSpanElement
+  onClick(sheet: Sheet): void
+}
