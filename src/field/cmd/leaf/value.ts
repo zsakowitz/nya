a1da4ed6cee74cbd0287720ece545efa4e9d46ce
sakawi <65452710+zsakowitz@users.noreply.github.com>
2025-03-07 12:32:30 -0800

    add type params to CmdValue

--- src/field/cmd/leaf/value.ts
+++ src/field/cmd/leaf/value.ts
@@ -1,14 +1,17 @@
 import { Leaf } from "."
 import type { Node } from "../../../eval/ast/token"
-import type { JsValue } from "../../../eval/ty"
+import type { JsValue, TyName } from "../../../eval/ty"
 import { h } from "../../../jsx"
 
 /**
  * Should only be used for `pick-cursor`. If this is displayed to the user, they
  * will be very confused, and something has probably gone very wrong.
  */
-export class CmdValue extends Leaf {
-  constructor(readonly value: JsValue) {
+export class CmdValue<
+  T extends TyName = TyName,
+  L extends number | false = number | false,
+> extends Leaf {
+  constructor(readonly value: JsValue<T, L>) {
     super("\\nyavalue", h("", ""))
   }
 
