2a650d681f77eeef1a6946cb8bf0ae0e637613c5
sakawi <65452710+zsakowitz@users.noreply.github.com>
2025-03-21 00:43:21 -0700

    units mostly work

--- src/pkg/unit/impl/system.ts
+++ src/pkg/unit/impl/system.ts
@@ -177,6 +177,36 @@ export function factor(src: UnitList, dst: UnitList): ConversionFactor {
   }
 }
 
+const HELP = `Try:
+1. If you mean a change in the measurement, prefix the variable with delta- (e.g. 1 deltafahrenheit means "a change of a single degree on the Fahrenheit scale")
+2. If you actually want a measurement, convert to an absolute scale (e.g. kelvin) using the "in" operator.`
+
+export function check(list: UnitList) {
+  list = list.filter((x) => num(x.exp) != 0)
+
+  let offset: UnitEntry | undefined
+
+  for (const unit of list) {
+    if (unit.unit.base.offset && num(unit.unit.base.offset) != 0) {
+      if (num(unit.exp) != 1) {
+        throw new Error(
+          `'${unit.unit.label}' can't be raised to any exponent besides one. ${HELP}`,
+        )
+      }
+
+      if (offset) {
+        throw new Error(
+          `Only one non-absolute unit is allowed in a compound unit, but two were used (${offset.unit.label} and ${unit.unit.label}). ${HELP}`,
+        )
+      }
+
+      offset = unit
+    }
+  }
+
+  return list
+}
+
 export function multiply(a: UnitList, b: UnitList): UnitList {
   const map = new Map<Unit, SReal>()
 
@@ -185,11 +215,11 @@ export function multiply(a: UnitList, b: UnitList): UnitList {
     map.set(entry.unit, existing ? add(existing, entry.exp) : entry.exp)
   }
 
-  return [...map].map(([unit, exp]) => ({ exp, unit }))
+  return check([...map].map(([unit, exp]) => ({ exp, unit })))
 }
 
 export function inv(a: UnitList): UnitList {
-  return a.map(({ exp, unit }) => ({ exp: neg(exp), unit }))
+  return check(a.map(({ exp, unit }) => ({ exp: neg(exp), unit })))
 }
 
 export function exp(a: UnitList, to: SReal): UnitList {
@@ -197,5 +227,5 @@ export function exp(a: UnitList, to: SReal): UnitList {
     return []
   }
 
-  return a.map(({ exp, unit }) => ({ exp: mul(exp, to), unit }))
+  return check(a.map(({ exp, unit }) => ({ exp: mul(exp, to), unit })))
 }
--- src/pkg/unit/pkg.ts
+++ src/pkg/unit/pkg.ts
@@ -7,6 +7,8 @@ import type { JsValue } from "@/eval/ty"
 import { num, real } from "@/eval/ty/create"
 import type { Display } from "@/eval/ty/display"
 import type { TyInfoByName } from "@/eval/ty/info"
+import { add, div, mul, neg, raise, sub } from "@/eval/ty/ops"
+import { CmdNum } from "@/field/cmd/leaf/num"
 import { OpCdot } from "@/field/cmd/leaf/op"
 import { CmdWord } from "@/field/cmd/leaf/word"
 import { CmdFrac } from "@/field/cmd/math/frac"
@@ -14,23 +16,45 @@ import { CmdSupSub } from "@/field/cmd/math/supsub"
 import { Block, L, R } from "@/field/model"
 import { h, sx } from "@/jsx"
 import type { Package } from ".."
-import { OP_CDOT, OP_DIV, OP_RAISE } from "../core/ops"
-import { exp, inv, multiply, type UnitList } from "./impl/system"
+import {
+  OP_ADD,
+  OP_CDOT,
+  OP_DIV,
+  OP_NEG,
+  OP_POS,
+  OP_RAISE,
+  OP_SUB,
+} from "../core/ops"
+import {
+  convert,
+  exp,
+  factor,
+  inv,
+  multiply,
+  type UnitList,
+} from "./impl/system"
 import { nan, UNITS_BY_NAME } from "./impl/units"
 
 declare module "@/eval/ty" {
   interface Tys {
     unit: UnitList
+    r32u: [SReal, UnitList]
   }
 
   interface TyComponents {
     unit: never
+    r32u: never
   }
 }
 
 const glsl = issue("Cannot use units in shaders yet.")
 
-function display(list: UnitList, display: Display) {
+function displayUnitPart(list: UnitList, display: Display, frac: boolean) {
+  if (list.length == 0 && frac) {
+    new CmdNum("1").insertAt(display.cursor, L)
+    return
+  }
+
   let first = true
   for (const {
     exp,
@@ -52,6 +76,25 @@ function display(list: UnitList, display: Display) {
   }
 }
 
+function displayUnit(list: UnitList, props: Display, require: boolean) {
+  const high = list.filter((x) => !(num(x.exp) < 0))
+  const low = inv(list.filter((x) => num(x.exp) < 0))
+
+  if (low.length != 0) {
+    const num = new Block(null)
+    displayUnitPart(high, props.at(num.cursor(R)), true)
+    const denom = new Block(null)
+    displayUnitPart(low, props.at(denom.cursor(R)), true)
+    new CmdFrac(num, denom).insertAt(props.cursor, L)
+  } else if (high.length == 0) {
+    if (require) {
+      new CmdWord("unitless", "var").insertAt(props.cursor, L)
+    }
+  } else {
+    displayUnitPart(high, props, false)
+  }
+}
+
 const INFO_UNIT: TyInfoByName<"unit"> = {
   name: "unit",
   namePlural: "units",
@@ -65,21 +108,77 @@ const INFO_UNIT: TyInfoByName<"unit"> = {
       return glsl()
     },
   },
-  coerce: {},
+  coerce: {
+    r32u: {
+      js(self) {
+        return [real(1), self]
+      },
+      glsl,
+    },
+  },
   write: {
     display(value, props) {
-      const high = value.filter((x) => !(num(x.exp) < 0))
-      const low = inv(value.filter((x) => num(x.exp) < 0))
+      displayUnit(value, props, true)
+    },
+    isApprox() {
+      return false
+    },
+  },
+  order: null,
+  point: false,
+  icon() {
+    return h(
+      "",
+      h(
+        "text-[#00786F] size-[26px] mb-[2px] mx-[2.5px] align-middle text-[16px] bg-[--nya-bg] inline-block relative border-2 border-current rounded-[4px]",
+        h(
+          "opacity-25 block w-full h-full bg-current absolute inset-0 rounded-[2px]",
+        ),
+        sx(
+          "svg",
+          {
+            class:
+              "size-[16px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 overflow-visible",
+            viewBox: "0 0 16 16",
+            fill: "none",
+          },
+          sx("path", {
+            d: "M 5 0 L 16 11 L 11 16 L 0 5 Z M 5.5 10.5 l 3 -3 M 2.75 7.75 l 1.5 -1.5 M 8.25 13.25 l 1.5 -1.5",
+            stroke: "currentcolor",
+            "stroke-linecap": "round",
+            "stroke-linejoin": "round",
+            "stroke-width": 2,
+          }),
+        ),
+      ),
+    )
+  },
+  // TODO: token changes depending on unit
+  token: null,
+  glide: null,
+  preview: null,
+  components: null,
+  extras: null,
+}
 
-      if (low.length == 0) {
-        display(high, props)
-      } else {
-        const num = new Block(null)
-        display(high, props.at(num.cursor(R)))
-        const denom = new Block(null)
-        display(low, props.at(denom.cursor(R)))
-        new CmdFrac(num, denom).insertAt(props.cursor, L)
-      }
+const INFO_R32U: TyInfoByName<"r32u"> = {
+  name: "real number with a unit",
+  namePlural: "real numbers with units",
+  get glsl() {
+    return glsl()
+  },
+  toGlsl: glsl,
+  garbage: {
+    js: [real(NaN), [{ exp: real(1), unit: nan }]],
+    get glsl() {
+      return glsl()
+    },
+  },
+  coerce: {},
+  write: {
+    display([value, unit], props) {
+      props.num(value)
+      displayUnit(unit, props, false)
     },
     isApprox() {
       return false
@@ -129,6 +228,13 @@ OP_CDOT.add(
   glsl,
 )
 
+OP_CDOT.add(
+  ["r32u", "r32u"],
+  "r32u",
+  (a, b) => [mul(a.value[0], b.value[0]), multiply(a.value[1], b.value[1])],
+  glsl,
+)
+
 OP_DIV.add(
   ["unit", "unit"],
   "unit",
@@ -136,7 +242,50 @@ OP_DIV.add(
   glsl,
 )
 
+OP_DIV.add(
+  ["r32u", "r32u"],
+  "r32u",
+  (a, b) => [
+    div(a.value[0], b.value[0]),
+    multiply(a.value[1], inv(b.value[1])),
+  ],
+  glsl,
+)
+
 OP_RAISE.add(["unit", "r32"], "unit", (a, b) => exp(a.value, b.value), glsl)
+
+OP_RAISE.add(
+  ["r32u", "r32"],
+  "r32u",
+  (a, b) => [raise(a.value[0], b.value), exp(a.value[1], b.value)],
+  glsl,
+)
+
+OP_ADD.add(
+  ["r32u", "r32u"],
+  "r32u",
+  ({ value: [v1, u1] }, { value: [v2, u2] }) => {
+    const f21 = factor(u2, u1)
+    return [add(v1, convert(v2, f21)), u1]
+  },
+  glsl,
+)
+
+OP_SUB.add(
+  ["r32u", "r32u"],
+  "r32u",
+  ({ value: [v1, u1] }, { value: [v2, u2] }) => {
+    const f21 = factor(u2, u1)
+    return [sub(v1, convert(v2, f21)), u1]
+  },
+  glsl,
+)
+
+OP_POS.add(["unit"], "unit", (a) => a.value, glsl)
+
+OP_POS.add(["r32u"], "r32u", (a) => a.value, glsl)
+
+OP_NEG.add(["r32u"], "r32u", ({ value: [a, u] }) => [neg(a), u], glsl)
 
 export const PKG_UNITS: Package = {
   id: "nya:units",
@@ -145,6 +294,17 @@ export const PKG_UNITS: Package = {
   ty: {
     info: {
       unit: INFO_UNIT,
+      r32u: INFO_R32U,
+    },
+    coerce: {
+      r32: {
+        r32u: {
+          js(self) {
+            return [self, []]
+          },
+          glsl,
+        },
+      },
     },
   },
   eval: {
