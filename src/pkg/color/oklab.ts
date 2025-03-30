c97d82b67bbc371f78c6a96adaf2250351a25db4
sakawi <65452710+zsakowitz@users.noreply.github.com>
2025-03-30 15:00:49 -0700

    fixed oklab

--- src/pkg/color/oklab.ts
+++ src/pkg/color/oklab.ts
@@ -2,6 +2,15 @@ import type { GlslContext } from "@/eval/lib/fn"
 
 export function declareOklab(ctx: GlslContext) {
   ctx.glsl`// https://github.com/patriciogonzalezvivo/lygia/blob/main/color/space/oklab2rgb.glsl
+#define SRGB_EPSILON 1e-10
+#define saturate(V) clamp(V, 0.0, 1.0)
+
+float rgb2srgb(const in float c) {   return (c < 0.0031308) ? c * 12.92 : 1.055 * pow(c, 0.4166666666666667) - 0.055; }
+vec3  rgb2srgb(const in vec3 rgb) {  return saturate(vec3(  rgb2srgb(rgb.r - SRGB_EPSILON), 
+                                                            rgb2srgb(rgb.g - SRGB_EPSILON), 
+                                                            rgb2srgb(rgb.b - SRGB_EPSILON))); }
+vec4  rgb2srgb(const in vec4 rgb) {  return vec4(rgb2srgb(rgb.rgb), rgb.a); }
+
 const mat3 _helper_oklab_OKLAB2RGB_A = mat3(
   1.0,           1.0,           1.0,
   0.3963377774, -0.1055613458, -0.0894841775,
@@ -12,7 +21,7 @@ const mat3 _helper_oklab_OKLAB2RGB_B = mat3(
   0.2309699292, -0.3413193965, 1.7076147010);
 vec3 _helper_oklab(const in vec3 oklab) {
   vec3 lms = _helper_oklab_OKLAB2RGB_A * oklab;
-  return _helper_oklab_OKLAB2RGB_B * (lms * lms * lms);
+  return rgb2srgb( _helper_oklab_OKLAB2RGB_B * (lms * lms * lms));
 }
 `
 }
