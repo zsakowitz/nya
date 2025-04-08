52553e95a213039ca765e23a03ef6c1f5df1e08d
sakawi <65452710+zsakowitz@users.noreply.github.com>
2025-04-07 22:16:28 -0600

    add zeta; remove its gl impl

--- src/pkg/special.ts
+++ src/pkg/special.ts
@@ -1,16 +1,12 @@
 import { FnDist } from "@/eval/ops/dist"
 import { approx, num, pt, real } from "@/eval/ty/create"
-import gammaGl from "@/glsl/gamma.glsl"
-import zetaGl from "@/glsl/zeta.glsl"
 import { complex, zeta } from "mathjs"
 import type { Package } from "."
-import { declareFactorialR32 } from "./factorial"
-import { declareDiv, declareMulC32, declarePowC32 } from "./num/complex"
 
 export const FN_ZETA: FnDist = new FnDist(
   "zeta",
   "computes the Riemann zeta function",
-).add(
+).addJs(
   ["c32"],
   "c32",
   (a) => {
@@ -20,18 +16,7 @@ export const FN_ZETA: FnDist = new FnDist(
     }
     return pt(approx(val.re), approx(val.im))
   },
-  (ctx, a) => {
-    declareMulC32(ctx)
-    declareDiv(ctx)
-    declarePowC32(ctx)
-    declareFactorialR32(ctx)
-    ctx.glslText(gammaGl)
-    ctx.glslText(
-      "vec2 powcx(vec2 x, vec2 y) { return _helper_pow_c32(x, y); }\n",
-    )
-    ctx.glslText(zetaGl)
-    return `helper_zeta_c32(${a.expr})`
-  },
+  "zeta2≈1.645",
 )
 
 export const PKG_SPECIAL_FNS: Package = {
