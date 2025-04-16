5fa118ae77c6c259e9097eab8d3201c9bcf2ca70
sakawi <65452710+zsakowitz@users.noreply.github.com>
2025-04-15 22:20:30 -0700

    indent addons

--- src/addons.ts
+++ src/addons.ts
@@ -4,76 +4,77 @@ import type { PackageIndex } from "#/manifest/types"
 import { makeDocName } from "./docs/util"
 import { h, hx } from "./jsx"
 
-const pkgs = manifest.packages
-  .map((x, i) => [...x, i] as const)
-  .filter((x) => x[0] in addons)
+export function createAddons() {
+  const pkgs = manifest.packages
+    .map((x, i) => [...x, i] as const)
+    .filter((x) => x[0] in addons)
 
-function has(id: string) {
-  return (
-    new URLSearchParams(location.search)
-      .get("addons")
-      ?.split(",")
-      .includes(id) ?? false
-  )
-}
-
-function set(id: string, on: boolean) {
-  const params = new URLSearchParams(location.search)
-  const addons = new Set(params.get("addons")?.split(",") ?? [])
-  if (on) {
-    addons.add(id)
-  } else {
-    addons.delete(id)
+  function has(id: string) {
+    return (
+      new URLSearchParams(location.search)
+        .get("addons")
+        ?.split(",")
+        .includes(id) ?? false
+    )
   }
-  const value = [...addons].join(",")
-  if (value) {
-    params.set("addons", value)
-  } else {
-    params.delete("addons")
+
+  function set(id: string, on: boolean) {
+    const params = new URLSearchParams(location.search)
+    const addons = new Set(params.get("addons")?.split(",") ?? [])
+    if (on) {
+      addons.add(id)
+    } else {
+      addons.delete(id)
+    }
+    const value = [...addons].join(",")
+    if (value) {
+      params.set("addons", value)
+    } else {
+      params.delete("addons")
+    }
+    history.replaceState(
+      {},
+      "",
+      location.origin + (params.toString() ? "?" + params : ""),
+    )
   }
-  history.replaceState(
-    {},
-    "",
-    location.origin + (params.toString() ? "?" + params : ""),
-  )
-}
 
-const fns = Object.entries(manifest.fns).flatMap(([k, v]) =>
-  v.map((v) => [k, v] as const),
-)
-
-const els = pkgs.map(([id, name, colorL, colorM, colorD, label, index]) => {
-  const el = hx(
-    "button",
-    {
-      class:
-        "grid grid-cols-[40%_auto] bg-[--nya-bg] border border-[--nya-border] text-[--d] px-3 py-2 rounded-lg text-[--nya-text-prose] gap-x-4 text-left items-start" +
-        (has(id) ? " nya-sx" : ""),
-      style: `--l:${colorL};--m:${colorM};--d:${colorD}`,
-    },
-    h(
-      "flex flex-col",
-      h("row-1 col-[1] text-[--nya-text] text-lg font-semibold", name),
-      h("row-2 col-[1] text-[--nya-title]", label),
-    ),
-    h(
-      "text-lg",
-      ...fns
-        .filter((x) => x[1][2].includes(index as PackageIndex))
-        .flatMap((x, i) => [
-          i == 0 ? null : h("font-['Symbola'] text-[1.265em]", ", "),
-          makeDocName(x[0]),
-        ]),
-    ),
+  const fns = Object.entries(manifest.fns).flatMap(([k, v]) =>
+    v.map((v) => [k, v] as const),
   )
 
-  el.addEventListener("click", () => {
-    set(id, !has(id))
-    el.classList.toggle("nya-sx", has(id))
+  const els = pkgs.map(([id, name, colorL, colorM, colorD, label, index]) => {
+    const el = hx(
+      "button",
+      {
+        class:
+          "grid grid-cols-[40%_auto] bg-[--nya-bg] border border-[--nya-border] text-[--d] px-3 py-2 rounded-lg text-[--nya-text-prose] gap-x-4 text-left items-start" +
+          (has(id) ? " nya-sx" : ""),
+        style: `--l:${colorL};--m:${colorM};--d:${colorD}`,
+      },
+      h(
+        "flex flex-col",
+        h("row-1 col-[1] text-[--nya-text] text-lg font-semibold", name),
+        h("row-2 col-[1] text-[--nya-title]", label),
+      ),
+      h(
+        "text-lg",
+        ...fns
+          .filter((x) => x[1][2].includes(index as PackageIndex))
+          .flatMap((x, i) => [
+            i == 0 ? null : h("font-['Symbola'] text-[1.265em]", ", "),
+            makeDocName(x[0]),
+          ]),
+      ),
+    )
+
+    el.addEventListener("click", () => {
+      set(id, !has(id))
+      el.classList.toggle("nya-sx", has(id))
+    })
+
+    return el
   })
 
-  return el
-})
-
-document.body.classList.add("p-4")
-document.body.appendChild(h("flex flex-col gap-2 max-w-3xl mx-auto", ...els))
+  return h("flex flex-col", ...els)
+}
