998f85eccb11b2035ee3377c60e289b136058cfd
sakawi <65452710+zsakowitz@users.noreply.github.com>
2025-05-13 08:40:07 -0700

    delete `emit/`

--- lang/src/emit/props.ts
+++
@@ -1,8 +0,0 @@
-export type Lang =
-  | "js:native" // uses JS's native floating-point numbers
-  | "js:native-tests"
-  | "glsl"
-
-export class EmitProps {
-  constructor(readonly lang: Lang) {}
-}
