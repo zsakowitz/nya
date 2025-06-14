d302f48ec9cbf8fbf25b441a80cbe570ff24ce1e
sakawi <65452710+zsakowitz@users.noreply.github.com>
2025-06-14 01:22:30 -0700

    remove preload script

--- pkg/script-index.ts
+++ pkg/script-index.ts
@@ -2,38 +2,36 @@ import s0 from "./scripts/real.nya"
 import s1 from "./scripts/complex.nya"
 import s2 from "./scripts/point.nya"
 import s3 from "./scripts/color.nya"
-import s4 from "./scripts/preload.nya"
-import s5 from "./scripts/erf.nya"
-import s6 from "./scripts/geometry.nya"
-import s7 from "./scripts/gamma.nya"
-import s8 from "./scripts/data/distributions.nya"
-import s9 from "./scripts/4d/two-complex.nya"
-import s10 from "./scripts/4d/point.nya"
-import s11 from "./scripts/color/extras.nya"
-import s12 from "./scripts/color/oklab.nya"
-import s13 from "./scripts/color/core.nya"
-import s14 from "./scripts/core/cmp.nya"
-import s15 from "./scripts/core/ops.nya"
-import s16 from "./scripts/3d/point.nya"
+import s4 from "./scripts/erf.nya"
+import s5 from "./scripts/geometry.nya"
+import s6 from "./scripts/gamma.nya"
+import s7 from "./scripts/data/distributions.nya"
+import s8 from "./scripts/4d/two-complex.nya"
+import s9 from "./scripts/4d/point.nya"
+import s10 from "./scripts/color/extras.nya"
+import s11 from "./scripts/color/oklab.nya"
+import s12 from "./scripts/color/core.nya"
+import s13 from "./scripts/core/cmp.nya"
+import s14 from "./scripts/core/ops.nya"
+import s15 from "./scripts/3d/point.nya"
 
 export const SCRIPTS = new Map([
   ["real", s0],
   ["complex", s1],
   ["point", s2],
   ["color", s3],
-  ["preload", s4],
-  ["erf", s5],
-  ["geometry", s6],
-  ["gamma", s7],
-  ["data/distributions", s8],
-  ["4d/two-complex", s9],
-  ["4d/point", s10],
-  ["color/extras", s11],
-  ["color/oklab", s12],
-  ["color/core", s13],
-  ["core/cmp", s14],
-  ["core/ops", s15],
-  ["3d/point", s16],
+  ["erf", s4],
+  ["geometry", s5],
+  ["gamma", s6],
+  ["data/distributions", s7],
+  ["4d/two-complex", s8],
+  ["4d/point", s9],
+  ["color/extras", s10],
+  ["color/oklab", s11],
+  ["color/core", s12],
+  ["core/cmp", s13],
+  ["core/ops", s14],
+  ["3d/point", s15],
 ])
 
 export type ScriptName =
@@ -41,7 +39,6 @@ export type ScriptName =
   | "complex"
   | "point"
   | "color"
-  | "preload"
   | "erf"
   | "geometry"
   | "gamma"
