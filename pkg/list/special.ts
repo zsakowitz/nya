3fd72d8d8d534563297f12e62d5eb45447750c1e
sakawi <65452710+zsakowitz@users.noreply.github.com>
2025-06-15 17:23:08 -0700

    add beta

--- pkg/list/special.ts
+++ pkg/list/special.ts
@@ -1,12 +1,6 @@
-import betaGl from "#/glsl/beta.glsl"
 import erfC32Gl from "#/glsl/erf-c32.glsl"
-import lngammaGl from "#/glsl/lngamma.glsl"
 import type { Package } from "#/types"
-import type { GlslContext } from "@/eval/lib/fn"
 import { FnDist } from "@/eval/ops/dist"
-import { approx, int } from "@/lib/real"
-import gammaln from "@stdlib/math/base/special/gammaln"
-import { declareFactorialR32 } from "./factorial"
 import { declareMulC32 } from "./num/complex"
 import { faddeevaPt } from "./special/erf-complex"
 
@@ -22,64 +16,6 @@ const FN_FADDEEVA = new FnDist("faddeeva", "scaled complex error function").add(
   "faddeeva(2+3i)≈0.130+0.081i",
 )
 
-function declareBeta(ctx: GlslContext) {
-  declareFactorialR32(ctx)
-  ctx.glslText(lngammaGl)
-  ctx.glslText(betaGl)
-}
-
-const FN_BETA = new FnDist("beta", "beta function").add(
-  ["r32", "r32"],
-  "r32",
-  (ar, br) => {
-    const a = ar.value.num()
-    const b = br.value.num()
-    const ln = gammaln(a) + gammaln(b) - gammaln(a + b)
-    const s = (x: number) => (x > 0 || Math.floor(x) % 2 == 0 ? 1 : -1)
-    const sign = s(a) * s(b) * s(a + b)
-    return approx(Math.exp(ln) * sign)
-  },
-  (ctx, a, b) => {
-    declareBeta(ctx)
-    return `beta(${a.expr}, ${b.expr})`
-  },
-  ["beta(2,3)=\\frac{1}{12}", "beta(3.6,-4.1)≈0.381"],
-)
-
-const FN_SIGNBETA = new FnDist("signbeta", "sign of the beta function").add(
-  ["r32", "r32"],
-  "r32",
-  (ar, br) => {
-    const a = ar.value.num()
-    const b = br.value.num()
-    const s = (x: number) => (x > 0 || Math.floor(x) % 2 == 0 ? 1 : -1)
-    return int(s(a) * s(b) * s(a + b))
-  },
-  (ctx, a, b) => {
-    declareBeta(ctx)
-    return `beta_sign(${a.expr}, ${b.expr})`
-  },
-  ["signbeta(2,3)=1", "signbeta(3.6,-4.1)≈-1"],
-)
-
-const FN_LNBETA = new FnDist(
-  "lnbeta",
-  "natural logarithm of the absolute value of the beta function",
-).add(
-  ["r32", "r32"],
-  "r32",
-  (ar, br) => {
-    const a = ar.value.num()
-    const b = br.value.num()
-    return approx(gammaln(a) + gammaln(b) - gammaln(a + b))
-  },
-  (ctx, a, b) => {
-    declareBeta(ctx)
-    return `beta_ln(${a.expr}, ${b.expr})`
-  },
-  ["lnbeta(3.6,-4.1)≈-0.963"],
-)
-
 export default {
   name: "special functions",
   label: "for uncomputable integrals and sums",
@@ -92,13 +28,10 @@ export default {
     "factorial",
     "gamma",
   ],
-  scripts: ["complex/zeta"],
+  scripts: ["complex/zeta", "complex/beta"],
   eval: {
     fn: {
       faddeeva: FN_FADDEEVA,
-      beta: FN_BETA,
-      lnbeta: FN_LNBETA,
-      signbeta: FN_SIGNBETA,
     },
   },
 } satisfies Package
--- pkg/script-index.ts
+++ pkg/script-index.ts
@@ -10,20 +10,21 @@ import s8 from "../nya/color/core.nya"
 import s9 from "../nya/real/parity.nya"
 import s10 from "../nya/real/index.nya"
 import s11 from "../nya/real/erf.nya"
-import s12 from "../nya/real/zeta.nya"
-import s13 from "../nya/real/number-theory.nya"
-import s14 from "../nya/complex/index.nya"
-import s15 from "../nya/complex/trig-hyperbolic.nya"
-import s16 from "../nya/complex/trig.nya"
-import s17 from "../nya/complex/number-theory.nya"
-import s18 from "../nya/core/cmp.nya"
-import s19 from "../nya/core/ops.nya"
-import s20 from "../nya/3d/point.nya"
-import s21 from "../nya/gamma/lngamma.nya"
-import s22 from "../nya/gamma/trigamma.nya"
-import s23 from "../nya/gamma/digamma.nya"
-import s24 from "../nya/gamma/polygamma.nya"
-import s25 from "../nya/gamma/gamma.nya"
+import s12 from "../nya/real/number-theory.nya"
+import s13 from "../nya/complex/index.nya"
+import s14 from "../nya/complex/trig-hyperbolic.nya"
+import s15 from "../nya/complex/trig.nya"
+import s16 from "../nya/complex/beta.nya"
+import s17 from "../nya/complex/zeta.nya"
+import s18 from "../nya/complex/number-theory.nya"
+import s19 from "../nya/core/cmp.nya"
+import s20 from "../nya/core/ops.nya"
+import s21 from "../nya/3d/point.nya"
+import s22 from "../nya/gamma/lngamma.nya"
+import s23 from "../nya/gamma/trigamma.nya"
+import s24 from "../nya/gamma/digamma.nya"
+import s25 from "../nya/gamma/polygamma.nya"
+import s26 from "../nya/gamma/gamma.nya"
 
 export const SCRIPTS = new Map([
   ["point", s0],
@@ -38,20 +39,21 @@ export const SCRIPTS = new Map([
   ["real/parity", s9],
   ["real", s10],
   ["real/erf", s11],
-  ["real/zeta", s12],
-  ["real/number-theory", s13],
-  ["complex", s14],
-  ["complex/trig-hyperbolic", s15],
-  ["complex/trig", s16],
-  ["complex/number-theory", s17],
-  ["core/cmp", s18],
-  ["core/ops", s19],
-  ["3d/point", s20],
-  ["gamma/lngamma", s21],
-  ["gamma/trigamma", s22],
-  ["gamma/digamma", s23],
-  ["gamma/polygamma", s24],
-  ["gamma/gamma", s25],
+  ["real/number-theory", s12],
+  ["complex", s13],
+  ["complex/trig-hyperbolic", s14],
+  ["complex/trig", s15],
+  ["complex/beta", s16],
+  ["complex/zeta", s17],
+  ["complex/number-theory", s18],
+  ["core/cmp", s19],
+  ["core/ops", s20],
+  ["3d/point", s21],
+  ["gamma/lngamma", s22],
+  ["gamma/trigamma", s23],
+  ["gamma/digamma", s24],
+  ["gamma/polygamma", s25],
+  ["gamma/gamma", s26],
 ])
 
 export type ScriptName =
@@ -67,11 +69,12 @@ export type ScriptName =
   | "real/parity"
   | "real"
   | "real/erf"
-  | "real/zeta"
   | "real/number-theory"
   | "complex"
   | "complex/trig-hyperbolic"
   | "complex/trig"
+  | "complex/beta"
+  | "complex/zeta"
   | "complex/number-theory"
   | "core/cmp"
   | "core/ops"
