b98d2bfebebfcc96f04b2338511a3b96d38cb3ef
sakawi <65452710+zsakowitz@users.noreply.github.com>
2025-07-08 13:44:40 -0400

    surreal numbers work better

--- src/field/cmd/math/brack.ts
+++ src/field/cmd/math/brack.ts
@@ -357,7 +357,20 @@ export class CmdBrack extends Command<[Block]> {
         ) ?
           ((parser.i += lhsRaw.length), lhsRaw.slice(1) as ParenLhs)
         : null
-      const contents = parser.until("\\right")
+
+      let contents: Block | undefined
+      let surrealLhsContents: Block | undefined
+      if (lhs == "{") {
+        const [contents1, end] = parser.untilAny(["\\right", "\\middle"])
+        if (end == "\\middle" && parser.peek() == "|") {
+          parser.i++ // consume the bar
+          surrealLhsContents = contents1
+        } else {
+          contents = contents1
+        }
+      }
+
+      contents ??= parser.until("\\right")
       const rhsRaw = parser.peek()
       const rhs =
         (
@@ -375,7 +388,9 @@ export class CmdBrack extends Command<[Block]> {
         ) ?
           ((parser.i += rhsRaw.length), rhsRaw.slice(1) as ParenRhs)
         : null
-      if (lhs && rhs) {
+      if (surrealLhsContents) {
+        return new CmdSurreal(surrealLhsContents, contents)
+      } else if (lhs && rhs) {
         return new CmdBrack(lhs, rhs, null, contents)
       } else if (lhs) {
         return new CmdBrack(lhs, matchParen(lhs), null, contents)
@@ -555,5 +570,107 @@ export class CmdBrack extends Command<[Block]> {
     } else if (dir == L && this.side == R && this[L]) {
       this.setSide(null)
     }
+  }
+}
+
+export class CmdSurreal extends Command<[Block, Block]> {
+  static fromLatex(_cmd: string, parser: LatexParser): Command {
+    const lhs = parser.arg()
+    const rhs = parser.arg()
+    return new CmdSurreal(lhs, rhs)
+  }
+
+  static render(lhs: Block, rhs: Block) {
+    const lhsSymbol = BRACKS["{"]
+    const rhsSymbol = BRACKS["}"]
+    const barSymbol = BRACKS["|"]
+    return h(
+      "inline-flex",
+      h("relative inline-block " + lhsSymbol.w, lhsSymbol.html()),
+      h("self-baseline my-[.1em]", lhs.el),
+      h("relative inline-block " + barSymbol.w, barSymbol.html()),
+      h("self-baseline my-[.1em]", rhs.el),
+      h("relative inline-block " + rhsSymbol.w, rhsSymbol.html()),
+    )
+  }
+
+  constructor(
+    readonly lhs: Block,
+    readonly rhs: Block,
+  ) {
+    super("\\left\\{", CmdSurreal.render(lhs, rhs), [lhs, rhs])
+  }
+
+  ascii(): string {
+    return `surreal(${this.blocks[0].ascii()};${this.blocks[1].ascii()})`
+  }
+
+  latex(): string {
+    return `\\left\\{${this.blocks[0].latex()}\\middle|${this.blocks[1].latex()}\\right\\}`
+  }
+
+  reader(): string {
+    return `SurrealLeft, ${this.blocks[0].reader()} SurrealRight, ${this.blocks[1].reader()} , EndSurreal ${this.rhs}`
+  }
+
+  moveInto(cursor: Cursor, towards: Dir): void {
+    if (towards == R) {
+      cursor.moveIn(this.blocks[0], L)
+    } else {
+      cursor.moveIn(this.blocks[1], R)
+    }
+  }
+
+  moveOutOf(cursor: Cursor, towards: Dir, block: Block): void {
+    if (block == this.blocks[0] && towards == R) {
+      cursor.moveIn(this.blocks[1], L)
+    } else if (block == this.blocks[1] && towards == L) {
+      cursor.moveIn(this.blocks[0], R)
+    } else {
+      cursor.moveTo(this, towards)
+    }
+  }
+
+  vertInto(): Block | undefined {
+    // TODO: make this better
+    return this.blocks[0]
+  }
+
+  vertFromSide(): undefined {}
+
+  vertOutOf(): undefined {}
+
+  delete(_cursor: Cursor, _from: Dir): void {
+    // TODO: whenever surreals are interactive make this better
+  }
+
+  deleteBlock(cursor: Cursor, at: Dir): void {
+    // TODO: make this better
+    cursor.moveTo(this, R)
+    this.remove()
+    cursor.insert(this.blocks[0], at == L ? R : L)
+  }
+
+  focus(x: number, y: number): Cursor {
+    const [lhs, rhs] = this.bounds()
+    if (x < lhs + this.em(BRACKS["{"].size / 2)) {
+      return this.cursor(L)
+    }
+    if (x > rhs - this.em(BRACKS["}"].size / 2)) {
+      return this.cursor(R)
+    }
+    const d0 = this.blocks[0].distanceTo(x)
+    const d1 = this.blocks[1].distanceTo(x)
+    return d0 <= d1 ? this.blocks[0].focus(x, y) : this.blocks[1].focus(x, y)
+  }
+
+  ir2(ret: IRBuilder): void {
+    ret.leaf({
+      type: "surreal",
+      data: {
+        lhs: this.blocks[0]!.parse(),
+        rhs: this.blocks[1]!.parse(),
+      },
+    })
   }
 }
