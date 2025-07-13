65e2f380fc42b088b3aed3f13b2d8c58ec6f85ce
sakawi <65452710+zsakowitz@users.noreply.github.com>
2025-07-13 19:21:11 -0400

    start on mobile keyboard

---
+++ src/keyboard/index.ts
@@ -0,0 +1,28 @@
+// Desmos keyboard is:
+// x   y  ^2  ^b    7 8 9 ÷    functions
+// (   )  <   >     4 5 6 *    left    right
+// ||  ,  <=  >=    1 2 3 -    delete
+// 🔤  🔊  √   a     0 . = +    enter
+
+// ABC goes to:
+// qwertyuiop
+// asdfghjklθ
+// shift zcvbnm backspace
+// num sub !% [] {} ~: ,' enter
+
+// Functions are:
+// trig 6
+// invtrig 6
+// stats
+// list ops
+// visualizations
+// probability distributions
+// inference
+// calculus
+// trigh
+// geometry
+// rgb, hsv
+// tone
+// number theory
+
+export function createKeyboard() {}
