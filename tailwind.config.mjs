f5c6991b9578e15715366e88ab808d1cc06d2ee1
sakawi <65452710+zsakowitz@users.noreply.github.com>
2025-07-17 23:13:30 -0400

    remove picking- utility since it's not used

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,33 +8,6 @@ export default {
       /** @type {import("tailwindcss/types/config").PluginAPI} */
       api,
     ) => {
-      api.matchVariant(
-        "picking",
-        (v) =>
-          v
-            .split(",")
-            .map((v) =>
-              v == "" ?
-                ":where(.nya-svg-display[data-nya-picking]) &"
-              : `:where(.nya-svg-display[data-nya-picking~="${api.e(v)}"]) &`,
-            ),
-        {
-          values: {
-            any: "",
-            point: "point32,point64",
-            line: "line,point32,point64",
-            ray: "ray,point32,point64",
-            segment: "segment,point32,point64",
-            vector: "vector",
-            polygon: "polygon",
-            circle: "circle,point32,point64",
-            angle: "angle",
-            directedangle: "directedangle",
-            arc: "arc,point32,point64",
-          },
-        },
-      )
-
       api.addVariant("any-hover", "@media (any-hover: hover)")
       api.addVariant("hover", "@media (any-hover: hover) { &:hover }")
     },
