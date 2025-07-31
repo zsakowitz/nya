d769a9d6821501e67ce68bc27d6aa5a2a8c17793
sakawi <65452710+zsakowitz@users.noreply.github.com>
2025-07-31 18:41:56 -0400

    add repl for experimentation

--- make/repl.ts
+++ make/repl.ts
@@ -2,16 +2,18 @@ import { ScriptEnvironment } from "!/exec/loader"
 import { errorText } from "@/error"
 import readline from "readline"
 import { ANSI } from "./nya/ansi"
+import repl from "./repl.nya"
 
 const env = new ScriptEnvironment()
+env._load("repl.nya", repl)
 
+console.write("\n> ")
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  terminal: false,
 })
-console.write("> ")
-rl.on("line", (line) => {
+
+for await (const line of rl) {
   try {
     if (/^(struct|enum|fn|let)\b/.test(line)) {
       env._load("repl", line)
@@ -19,11 +21,13 @@ rl.on("line", (line) => {
       const { block, value } = env.process(line)
       console.log(`${ANSI.cyan}${value.type}
 ${ANSI.blue}${block.source ? block.source + "\n" : ""}${value.toRuntime()}${ANSI.reset}`)
+      console.log(env.evalRaw(block.source + "\n" + value.toRuntime()))
     }
   } catch (e) {
     console.error(`${ANSI.red}${errorText(e)}${ANSI.reset}`)
   }
-  console.write("> ")
-})
+  console.write("\n> ")
+}
+
 // TODO: @length(vec2) seems to print @length(ans.x, ans.y, undefined); fix
 // TODO: @length(vec3) seems to print @length(ans.x, ans.y, ans.z, undefined, undefined); fix
