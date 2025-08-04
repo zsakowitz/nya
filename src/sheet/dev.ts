3aa066c7b014fc724334fa134a524198d0a59348
sakawi <65452710+zsakowitz@users.noreply.github.com>
2025-08-04 15:20:28 -0400

    move default srcs to src/assets/preload

--- src/sheet/dev.ts
+++ src/sheet/dev.ts
@@ -1,10 +1,11 @@
-import SRC_LOCALHOST from "@/assets/example/localhost.txt"
-import SRC_STANDARD from "@/assets/example/standard.txt"
+import SRC_LOCALHOST from "@/assets/preload/localhost.txt"
+import SRC_STANDARD_2D from "@/assets/preload/standard2d.txt"
+import SRC_STANDARD_3D from "@/assets/preload/standard3d.txt"
 import { options } from "@/field/defaults"
+import { showKeyboard } from "@/field/kbd/global"
 import { builtin, type PackageId } from "@/pkg"
 import { SheetFactory } from "./factory"
-
-import { showKeyboard } from "@/field/kbd/global"
+import { PLOT_3D } from "./plot/3d"
 
 const factory = new SheetFactory(options)
 
@@ -23,9 +24,11 @@ async function loadBy(key: string) {
   await load(ids.split(","))
 }
 
-if (globalThis.location?.search.includes("addons")) {
+const search = new URL(location.href).searchParams
+
+if (search.has("addons")) {
   await Promise.all([loadBy("addons"), loadBuiltin()])
-} else if (globalThis.location?.search.includes("onlypkg")) {
+} else if (search.has("onlypkg")) {
   await loadBy("onlypkg")
 } else {
   await loadBuiltin()
@@ -36,12 +39,15 @@ const IS_DEV = "NYA_DEV" in globalThis
 const sheet = factory.create()
 Object.assign(globalThis, { sheet })
 document.body.appendChild(sheet.el)
-if (globalThis.location?.href.includes("showkeyboard")) {
+if (search.has("showkeyboard")) {
   setTimeout(() => {
     showKeyboard(document.querySelector(".nya-kbd-field")!.nyaField!)
   }, 100)
 }
 
-const src = IS_DEV ? SRC_LOCALHOST : SRC_STANDARD
+const src =
+  IS_DEV ? SRC_LOCALHOST
+  : PLOT_3D ? SRC_STANDARD_3D
+  : SRC_STANDARD_2D
 
 src.split("\n").forEach((x) => x && sheet.list.fromString(x))
